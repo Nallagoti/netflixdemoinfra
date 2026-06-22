30b22ecfc6edc15b7913c98b947ed25794bf799c
Nallagoti <nallagotinissitha@gmail.com>
2026-06-22 17:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-2"
 }
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0ecb62995f68bb549"
+  ami                    = "ami-04a64102b8022e4f3"
   instance_type          = "c7i-flex.large"
   key_name               = "rahamdocker"
-  vpc_security_group_ids = ["sg-0594e10139ac78ef5"]
+  vpc_security_group_ids = ["sg-0bcdfca25bea871a3"]
   tags = {
     Name = var.instance_names[count.index]
   }
